1094e8d579d29a31134c348a0aa8cf99a4ae3fa3
jaystahh <bashy477@gmail.com>
2025-12-31 01:04:08 -0500

    Week 1: basic functions

---
+++ week1/functions.py
@@ -0,0 +1,9 @@
+def greet_player(name):
+    print(f"Hello {name}")
+
+def calculate_damage(damagenumber):
+    return damagenumber + 2
+
+greet_player("Jayden")
+damage = calculate_damage(5)
+print(damage)
