5bd4326b35e13751e5d0cc332ce70ef84d43588b
jaystahh <bashy477@gmail.com>
2025-12-31 00:11:08 -0500

    Week 1: user input and conditionals

---
+++ week1/input_conditionals.py
@@ -0,0 +1,12 @@
+print("What is your name?")
+name = input()
+print(f"Hello {name}")
+age = int(input("How old are you?"))
+if age < 13:
+    print("You are a child. You came from my balls.")
+
+elif age < 16:
+    print("You're almost 16 years old! Wow!")
+
+elif age < 20:
+    print(f"Happy birthday {name}. You're almost out of your teenage years!")
